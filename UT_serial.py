84d58412a7b675dace9390bcbf775317f9e2aba9
shiyang1 <shiyang1@xiaomi.com>
2015-01-05 11:24:33 +0800

    更新串口测试应用程序
    Signed-off-by: shiyang1 <shiyang1@xiaomi.com>

--- UT_serial.py
+++ UT_serial.py
@@ -2,7 +2,20 @@ __author__ = 'shiyang'
 #encoding:UTF-8
 
 import serial
+import time
 
 def Test_serial_main(name,speed):
     ser = serial.Serial(name,speed,timeout=2)
-    return ser.isOpen()
+    status = ser.isOpen()
+    if (status):
+        print("true")
+    else:
+        print("false")
+
+    ser.write("\n ls \n")
+    time.sleep(0.1)
+    cnt = ser.inWaiting()
+    str = ser.read(cnt)
+    print str
+
+    return
